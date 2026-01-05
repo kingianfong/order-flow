Fix off by one, clean up

models/point_process.py
@@ -8,6 +8,7 @@ import datetime
 from jax import Array
 from jax.flatten_util import ravel_pytree
 from jax.scipy.optimize import minimize
+from jax.scipy.stats import poisson
 from jax.typing import ArrayLike
 import jax
 import jax.numpy as jnp
@@ -22,6 +23,8 @@ DATA_DIR = Path(__file__).parent.parent / 'data/preprocessed'
 # %%
 
 
+# raw data only has sometimes has multiple trades per timestamp
+# this input dataset is grouped by millisecond
 def load_data(sym: str) -> pl.DataFrame:
     df = (
         pl.scan_parquet(DATA_DIR)
@@ -29,13 +32,14 @@ def load_data(sym: str) -> pl.DataFrame:
             pl.col('sym') == pl.lit(sym),
             pl.col('date') >= datetime.date(2025, 12, 1),
         )
-        .select('time', count=pl.col('total_count'))
-        .with_columns(
-            duration_precise=pl.col('time').diff().cast(pl.Duration('ns')),
+        .select(
+            pl.col('time'),
+            curr_count=pl.col('total_count'),
+            elapsed_precise=pl.col('time').diff().cast(pl.Duration('ns')),
         )
         .with_columns(
-            duration=(
-                pl.col('duration_precise')
+            elapsed=(
+                pl.col('elapsed_precise')
                 .cast(float) * 1e-6  # milliseconds
             ),
             hour=(
@@ -43,12 +47,13 @@ def load_data(sym: str) -> pl.DataFrame:
                 - pl.col('time').dt.truncate('1d')
             ).dt.total_hours(fractional=True),
         )
-        .filter(pl.col('duration').is_not_null())
+        .filter(pl.col('elapsed').is_not_null())
         .collect()
     )
-    assert (df['count'] > 0).all()
-    assert (df['duration'] > 0).all()
+    assert (df['curr_count'] > 0).all()
+    assert (df['elapsed'] > 0).all()
     assert (df['hour'] >= 0).all()
+    assert df['time'].is_unique().all()
     assert df['time'].is_sorted()
     return df
 
@@ -61,33 +66,31 @@ INPUT_DF
 
 
 class Dataset(NamedTuple):
-    counts: Array
-    durations: Array
-    times_of_day: Array
+    curr_count: Array
+    elapsed: Array
+    time_of_day: Array
 
 
 DATASET = Dataset(
-    counts=INPUT_DF['count'].to_jax(),
-    durations=INPUT_DF['duration'].to_jax(),
-    times_of_day=INPUT_DF['hour'].to_jax(),
+    curr_count=INPUT_DF['curr_count'].to_jax(),
+    elapsed=INPUT_DF['elapsed'].to_jax(),
+    time_of_day=INPUT_DF['hour'].to_jax(),
 )
 
 
 # %%
 
 
-closed_form_rate = DATASET.counts.sum() / DATASET.durations.sum()
+closed_form_rate = DATASET.curr_count.sum() / DATASET.elapsed.sum()
 closed_form_rate
 
 
 # %%
 
 
+# assumes rate is effectively constant
 def calc_loglik(rate: ArrayLike, dataset: Dataset) -> Array:
-    return jax.scipy.stats.poisson.logpmf(
-        k=dataset.counts,
-        mu=rate*dataset.durations,
-    )
+    return poisson.logpmf(k=dataset.curr_count, mu=rate*dataset.elapsed)
 
 
 def constant_rate_loss(params: Array, dataset: Dataset):
@@ -158,7 +161,7 @@ def plot_rbf_rate(params: RbfRateParams) -> None:
 
 init_rbf_params = RbfRateParams(
     base_log_rate=float(jnp.log(constant_rate)),
-    rbf_weights=jnp.sin(jnp.arange(N_CENTERS)),
+    rbf_weights=0.05 * jnp.sin(jnp.arange(N_CENTERS)),
 )
 
 
@@ -175,7 +178,7 @@ def rbf_rate_loss(flat_params: Array,
                   unflatten_fn: Callable,
                   dataset: Dataset):
     rbf_params = unflatten_fn(flat_params)
-    log_rate = calc_log_rate_rbf(rbf_params, dataset.times_of_day)
+    log_rate = calc_log_rate_rbf(rbf_params, dataset.time_of_day)
     rate = jnp.exp(log_rate)
     return -calc_loglik(rate, dataset).mean()
 
@@ -195,7 +198,7 @@ plot_rbf_rate(rbf_optim_params)
 # %%
 
 
-rbf_log_rate = calc_log_rate_rbf(rbf_optim_params, DATASET.times_of_day)
+rbf_log_rate = calc_log_rate_rbf(rbf_optim_params, DATASET.time_of_day)
 rbf_rate = jnp.exp(rbf_log_rate)
 
 
@@ -214,32 +217,43 @@ class HawkesParams(NamedTuple):
 
 
 class HawkesOutputs(NamedTuple):
-    decay_factors: Array
-    decayed_counts: Array
-    rates: Array
+    decay_factor: Array
+    decayed_count: Array
+    loglik: Array  # excludes events[t]
+    rate: Array  # includes events[t]
 
 
-@jax.jit
 def calc_hawkes(params: HawkesParams, dataset: Dataset) -> HawkesOutputs:
     base_rate = jnp.exp(params.base_log_rate)
     norm = jax.nn.sigmoid(params.logit_norm)
     omega = jnp.exp(params.log_omega)
-    decay_factors = jnp.exp(-omega * dataset.durations)
+
+    decay_factors = jnp.exp(-omega * dataset.elapsed)
 
     def step(carry, x):
-        decayed_counts = carry
-        count, decay_factor = x
-        decayed_counts *= decay_factor
-        decayed_counts += count
-        return decayed_counts, decayed_counts
+        decayed_count = carry
+        count, duration, decay_factor = x
 
-    _, decayed_counts = jax.lax.scan(step, 0, (dataset.counts, decay_factors))
-    rates = base_rate + norm * omega * decayed_counts
+        decayed_count *= decay_factor
+        loglik_rate = base_rate + norm * omega * decayed_count
+        loglik = poisson.logpmf(k=count, mu=loglik_rate * duration)
+
+        # assume all trades happen at the same time because they happen at
+        # the same millisecond (timestamp resolution)
+        decayed_count += count
+
+        forecast_rate = base_rate + norm * omega * decayed_count
+        return decayed_count, (decayed_count, loglik, forecast_rate)
+
+    xs = dataset.curr_count, dataset.elapsed, decay_factors
+    _, y = jax.lax.scan(step, 0, xs)
+    decayed_count, loglik, rate = y
 
     return HawkesOutputs(
-        decay_factors=decay_factors,
-        decayed_counts=decayed_counts,
-        rates=rates,
+        decay_factor=decay_factors,
+        decayed_count=decayed_count,
+        loglik=loglik,
+        rate=rate,
     )
 
 
@@ -257,10 +271,10 @@ def plot_hawkes_rate(params: HawkesParams,
     df = (
         input_df
         .with_columns(
-            decay_factors=np.asarray(outputs.decay_factors),
-            decayed_counts=np.asarray(outputs.decayed_counts),
-            rates=np.asarray(outputs.rates),
-
+            decay_factor=np.asarray(outputs.decay_factor),
+            decayed_count=np.asarray(outputs.decayed_count),
+            loglik=np.asarray(outputs.loglik),
+            rate=np.asarray(outputs.rate),
         )
     )
     display(df)
@@ -271,9 +285,10 @@ def plot_hawkes_rate(params: HawkesParams,
             pl.col('time') <= datetime.datetime(2025, 12, 13),
         )
     )
-    _, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-    ax1.scatter(subset['time'], subset[['count']], alpha=0.05, marker='.')
-    ax2.plot(subset['time'], subset[['rates']])
+    _, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+    ax1.scatter(subset['time'], subset[['curr_count']], alpha=0.05, marker='.')
+    ax2.plot(subset['time'], subset[['rate']])
+    ax3.plot(subset['time'], subset[['loglik']])
     plt.tight_layout()
     plt.show()
 
@@ -297,7 +312,7 @@ def hawkes_loss(flat_params: Array,
                 dataset: Dataset):
     hawkes_params = unflatten_fn(flat_params)
     output = calc_hawkes(hawkes_params, dataset)
-    return -calc_loglik(output.rates, dataset).mean()
+    return -output.loglik.mean()
 
 
 hawkes_optim_result = minimize(
@@ -326,9 +341,7 @@ result_df = (
         rbf_loglik=np.asarray(
             calc_loglik(rbf_rate, DATASET),
         ),
-        hawkes_loglik=np.asarray(
-            calc_loglik(hawkes_outputs.rates, DATASET),
-        ),
+        hawkes_loglik=np.asarray(hawkes_outputs.loglik),
     )
     .group_by('time', maintain_order=True)
     .sum()